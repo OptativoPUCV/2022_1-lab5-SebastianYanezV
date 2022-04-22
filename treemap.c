47cf3a1bf33182e6d0a5f05178399a8415f7a225
Your Name <you@example.com>
2022-04-22 11:58:15 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -155,14 +155,13 @@ void removeNode(TreeMap * tree, TreeNode* node)
         return;
     }
 
-    /*if (node->left != NULL && node->right != NULL)
+    if (node->left != NULL && node->right != NULL)
     {
         TreeNode *aux = minimum(node->right);
-        node->pair->key = aux->pair->key;
-        node->pair->value = aux->pair->value;
+        node->pair = aux->pair;
         removeNode(tree, aux);
         return;
-    }*/
+    }
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
